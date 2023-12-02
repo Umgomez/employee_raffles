c5c83d26307ba4a71b7b3c78718653da26d4a035
Ubaldin Gomez <ubaldin_gomez@claro.net.do>
2023-12-02 11:24:40 -0400

    Cambiando presentacion de la pagina principal

--- Models/Default/Awards/Awards.Entity.cs
+++ Models/Default/Awards/Awards.Entity.cs
@@ -1,7 +1,10 @@
-﻿namespace employee_raffles.Models.Default;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace employee_raffles.Models.Default;
 
 public class Awards
 {
+    [Key]
     public int ID { get; set; }
     public int Sequence { get; set; }
     public string Amount { get; set; }
--- Models/Default/Employee/Employees.Entity.cs
+++ Models/Default/Employee/Employees.Entity.cs
@@ -1,16 +1,18 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace employee_raffles.Models.Default;
 
 public class Employees
 {
+    [Key]
     public int EmpleadoID {  get; set; }
     public string Tarjeta { get; set; }
     public string Nombres { get; set; }
     public string Apellidos { get; set; }
     public string Cedula { get;  set; }
     public bool Asistencia { get; set; }
-    public int NumeroRifa { get; set; }
+    public int? NumeroRifa { get; set; }
     public bool SelRifa { get; set; }
     public DateTime FechaAsistencia { get; set; }
 
